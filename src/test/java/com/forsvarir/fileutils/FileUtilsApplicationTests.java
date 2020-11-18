f596d162fe9f66e47f700642aebd8b9c788e5252
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2020-11-18 20:53:29 +0000

    Initial commit of generated project.  JPA/database disabled in POM.

---
+++ src/test/java/com/forsvarir/fileutils/FileUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forsvarir.fileutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
