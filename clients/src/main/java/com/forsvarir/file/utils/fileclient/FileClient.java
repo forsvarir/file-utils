b3eb23692434acebe8e8558f5bbd7f9984c387c6
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2021-03-09 21:30:04 +0000

    Start using the application builder, so that we can specify that we don't want the web server to start.

--- clients/src/main/java/com/forsvarir/file/utils/fileclient/FileClient.java
+++ clients/src/main/java/com/forsvarir/file/utils/fileclient/FileClient.java
@@ -3,19 +3,23 @@ package com.forsvarir.file.utils.fileclient;
 import com.forsvarir.common.utils.file.FileWalker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import java.nio.file.Path;
 
 @SpringBootApplication
 public class FileClient implements CommandLineRunner {
     public static void main(String[] args) {
-        SpringApplication.run(FileClient.class, args);
+        new SpringApplicationBuilder(FileClient.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
     }
 
     @Override
     public void run(String... args) {
         FileWalker.walk(Path.of("/tmp"))
-                .forEach(f->System.out.println(f.toString()));
+                .forEach(f -> System.out.println(f.toString()));
     }
 }
