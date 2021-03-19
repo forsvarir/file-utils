f7e92d6c9968e2df37883a5830ce0f361728ce84
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2021-03-19 20:32:54 +0000

    Added simple smoke test to validate that file endpoint works.

--- services/src/test/java/com/forsvarir/file/utils/services/FileServicesApplicationIT.java
+++ services/src/test/java/com/forsvarir/file/utils/services/FileServicesApplicationIT.java
@@ -1,13 +1,30 @@
 package com.forsvarir.file.utils.services;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FileServicesApplicationIT {
+	public static final String FILE_SERVICE_URL = "http://localhost:8080/file-utils/files/0";
+
+	@Autowired
+	RestTemplateBuilder restTemplateBuilder;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void canHitFileEndPoint() {
+		var response = restTemplateBuilder
+				.build()
+				.getForEntity(FILE_SERVICE_URL, String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
 }
