7fe9379621f0a72813a3e87ca72d25fd6dc93771
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2021-03-19 19:51:54 +0000

    Renamed services application

--- services/src/main/java/com/forsvarir/file/utils/services/FileServicesApplication.java
+++ services/src/main/java/com/forsvarir/file/utils/services/FileServicesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FileUtilsApplication {
+public class FileServicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FileUtilsApplication.class, args);
+		SpringApplication.run(FileServicesApplication.class, args);
 	}
 
 }
